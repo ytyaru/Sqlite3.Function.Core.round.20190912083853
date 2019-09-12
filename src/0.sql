964fac9d38f7775a148e0dec9bccf9f928bc58b6
ytyaru <ytyaru@outlook.jp>
2019-09-12 09:00:20 +0900

    初回コミット

---
+++ src/0.sql
@@ -0,0 +1,11 @@
+.echo on
+select round(1.23456789);
+select round(1.23456789, 1);
+select round(1.23456789, 2);
+select round(1.23456789, 3);
+
+select round(4.4);
+select round(4.5);
+select round(5.4);
+select round(5.5);
+
